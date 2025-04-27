d1d9dace8581eac9378a8e0561f84bd3104c5efe
uvais724 <uvais724@gmail.com>
2025-04-27 15:44:50 +0530

    Added code to handle youtube videos

--- server/utils/linkScraper.ts
+++ server/utils/linkScraper.ts
@@ -1,4 +1,6 @@
 import puppeteer from 'puppeteer';
+import axios from 'axios';
+import { JSDOM } from 'jsdom'
 
 /**
  * Fetches preview data for a given URL using open-graph-scraper.
@@ -7,6 +9,10 @@ import puppeteer from 'puppeteer';
  * @returns {Promise<object>} - The preview data.
  */
 export async function fetchPreview(url: string) {
+  if (url.includes('youtube.com') || url.includes('youtu.be')) {
+    return await returnYoutubeMetadataForUrl(url);
+  }
+
   // Fallback to Puppeteer
   try {
     const browser = await puppeteer.launch({
@@ -60,4 +66,53 @@ export async function fetchPreview(url: string) {
     }
     throw new Error('Both open-graph-scraper and Puppeteer failed to fetch preview data.');
   }
+}
+
+function normalizeYouTubeUrl(url: string | URL) {
+  const parsed = new URL(url);
+
+  // If it's a shortened youtu.be link
+  if (parsed.hostname === 'youtu.be') {
+    const videoId = parsed.pathname.slice(1); // remove leading "/"
+    return `https://www.youtube.com/watch?v=${videoId}`;
+  }
+
+  // Otherwise, return the url as-is
+  return url;
+}
+
+async function returnYoutubeMetadataForUrl(url: string) {
+  let normalizedUrl = url;
+  if(url.includes('youtu.be')) {
+    normalizedUrl = normalizeYouTubeUrl(url).toString();
+    console.log('Normalized YouTube URL:', normalizedUrl);
+  }
+  
+  const { data: html } = await axios.get(normalizedUrl, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+      'Connection': 'keep-alive'
+    }
+    
+  });
+
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+  const getMetaContent = (property: string) => 
+    (document.querySelector(`meta[property="${property}"]`) as HTMLMetaElement)?.content || '';
+
+  const metadata = {
+    title: getMetaContent('og:title'),
+    description: getMetaContent('og:description'),
+    thumbnail: getMetaContent('og:image')
+  };
+  return {
+    title: metadata.title,
+    description: metadata.description,
+    image: metadata.thumbnail,
+    tool: 'YouTube oEmbed',
+    url: url,
+  };
 }
