09b05ed89352eda26391ae536ac1aa80038ae31f
uvais724 <uvais724@gmail.com>
2025-04-27 16:27:18 +0530

    Modifying headless option for youtube

--- server/utils/linkScraper.ts
+++ server/utils/linkScraper.ts
@@ -9,8 +9,9 @@ import { JSDOM } from 'jsdom'
  * @returns {Promise<object>} - The preview data.
  */
 export async function fetchPreview(url: string) {
+  let headless = true;
   if (url.includes('youtube.com') || url.includes('youtu.be')) {
-    return await returnYoutubeMetadataForUrl(url);
+    headless = false; // Disable headless mode for YouTube URLs
   }
 
   // Fallback to Puppeteer
@@ -25,7 +26,7 @@ export async function fetchPreview(url: string) {
         '--single-process',
         '--no-zygote',
       ],
-      headless: true
+      headless: headless
     });
     const page = await browser.newPage();
 
@@ -66,53 +67,4 @@ export async function fetchPreview(url: string) {
     }
     throw new Error('Both open-graph-scraper and Puppeteer failed to fetch preview data.');
   }
-}
-
-function normalizeYouTubeUrl(url: string | URL) {
-  const parsed = new URL(url);
-
-  // If it's a shortened youtu.be link
-  if (parsed.hostname === 'youtu.be') {
-    const videoId = parsed.pathname.slice(1); // remove leading "/"
-    return `https://www.youtube.com/watch?v=${videoId}`;
-  }
-
-  // Otherwise, return the url as-is
-  return url;
-}
-
-async function returnYoutubeMetadataForUrl(url: string) {
-  let normalizedUrl = url;
-  if(url.includes('youtu.be')) {
-    normalizedUrl = normalizeYouTubeUrl(url).toString();
-    console.log('Normalized YouTube URL:', normalizedUrl);
-  }
-  
-  const { data: html } = await axios.get(normalizedUrl, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-      'Connection': 'keep-alive'
-    }
-    
-  });
-
-  const dom = new JSDOM(html);
-  const document = dom.window.document;
-  const getMetaContent = (property: string) => 
-    (document.querySelector(`meta[property="${property}"]`) as HTMLMetaElement)?.content || '';
-
-  const metadata = {
-    title: getMetaContent('og:title'),
-    description: getMetaContent('og:description'),
-    thumbnail: getMetaContent('og:image')
-  };
-  return {
-    title: metadata.title,
-    description: metadata.description,
-    image: metadata.thumbnail,
-    tool: 'YouTube oEmbed',
-    url: url,
-  };
 }
